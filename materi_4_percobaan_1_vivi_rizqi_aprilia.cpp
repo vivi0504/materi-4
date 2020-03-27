3f1942dd784615122cfa29c6ee8a3ef107ea312c
vivi rizqi aprilia <61743519+vivi0504@users.noreply.github.com>
2020-03-27 17:39:02 +0700

    Add files via upload

---
+++ materi_4_percobaan_1_vivi_rizqi_aprilia.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int i;
+	
+	cout<<"menjalankan perulangan tanpa for"<<endl;
+	cout<<"menjalankan perulangan tanpa for"<<endl;
+	cout<<"menjalankan perulangan tanpa for"<<endl;
+	cout<<"menjalankan perulangan tanpa for"<<endl;
+	cout<<"menjalankan perulangan tanpa for"<<endl;
+	
+	for(int i=1;i<5;i++)
+	{
+		cout<<"mejalankan perulangan dengan for"<<endl;
+	}
+	
+	system("pause");
+	return 0;
+}
